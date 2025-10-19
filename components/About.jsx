68b953148fd6fbe77fb1ea1bd49384a75e76e393
Pavitra <pavi.kumar4002@gmail.com>
2025-10-19 20:45:23 -0300

    Update

--- components/About.jsx
+++ components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
                 <div className='col-span-2'>
                     <h2 className='py-4 border-b'> About</h2>
                     
-                    <p className='py-2 text-gray-600'>Hi! My name is Pavitra. I am a 3rd year Software Engineering student at the University of New Brunswick. I have more than 1.5 years of experience doing Software development, security analysis, and AI at companies and organizations like <b>IBM</b> and <b>Civic Tech Fredericton</b>. </p>
+                    <p className='py-2 text-gray-600'>Hi! My name is Pavitra. I am a 3rd year Software Engineering student at the University of New Brunswick. I have more than 1.5 years of experience doing Software development, Data Engineering and Analysis, and AI at companies and organizations like <b>IBM</b> and <b>Civic Tech Fredericton</b>. </p>
                     <p className='py-2 text-gray-600'> I have research experience with world-renowned researchers like Dr. Rongxing Lu. I am currently doing researching on the Detection of IoT DDoS attacks using Machine Learning algorithms. I am also doing research with Dr. Debasmita Mukherjee on Explainable AI systems. </p>
                     <p className='py-2 text-gray-600'>My hobbies include playing sports like Tennis. I also like singing differnt types of songs. I have made several singing performances at several events at the University of New Brunswick.</p>
                 </div>
